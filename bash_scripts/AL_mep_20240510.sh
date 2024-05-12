8a9af0cace93919d5037da67a617099aa248ffd7
Drew Prinster <46874730+drewprinster@users.noreply.github.com>
2024-05-12 19:44:41 -0400

    Bash scripts for running experiments with slurm

---
+++ bash_scripts/AL_mep_20240510.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#SBATCH -t 1:00:00 
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
+#SBATCH --mem=100G
+python ../run_SplitCP_MultistepFCS_ALexpts_20240510.py --dataset meps --n_steps 10 --n_train_initial 80 --lmbdas 12 --p_split_train 0.8 --noise_magnitude 0.05 --weight_adj_depth_maxes 1 2 3 4 5 6 --initial_sampling_bias 3.0 --add_to_train_split_prob 0.5 --noise_level 0.05 --n_seed 1 --sigma_0 0.05 --beta 0.0
